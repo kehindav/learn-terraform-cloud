d74672430b06940c8721fdde22c465fb2f5da8c8
kehindav <115744369+kehindav@users.noreply.github.com>
2024-01-09 18:27:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,6 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
-/* ======= */
+/* ======= 
 
->>>>>>> 4db4061226975960972abe0e2c6d47e92823d450
+>>>>>>> 4db4061226975960972abe0e2c6d47e92823d450 */
